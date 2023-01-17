44fb4428ef064e0059bdbcaf4face7971043cebd
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2023-01-17 17:46:36 +0800

    partially add processing latest block

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(improper_ctypes)]
+
+use marine_rs_sdk::marine;
+use marine_rs_sdk::module_manifest;
+use marine_rs_sdk::MountedBinaryResult;
+use marine_rs_sdk::WasmLoggerBuilder;
+use serde_json::json;
+
+module_manifest!();
+
+pub fn main() {
+    WasmLoggerBuilder::new().build().unwrap();
+}
+
+#[marine]
+pub fn process_latest_block(address: String) {
+    let last_block = 0;
+
+    let query = format!(
+        r#"query {{
+        mintBurnMints (
+            orderBy: blockNo,
+            orderDirection: asc,
+            where: {{
+                blockNo_gt: {}
+            }},
+        ) {{
+        id
+        to
+        blockNo
+    }}
+    }}"#,
+        last_block
+    );
+
+    let query_json = json!({ "query": query });
+    let json_str = serde_json::to_string(&query_json).unwrap();
+
+    let args = vec![
+        String::from("-X"),
+        String::from("POST"),
+        String::from("-H"),
+        String::from("Content-Type: application/json"),
+        String::from("-d"),
+        json_str,
+        address,
+    ];
+
+    let response = curl(args);
+    // convert to string - String::from_utf8(response.stdout)?;
+    // convert to json - serde_json::from_str();
+}
+
+#[marine]
+#[link(wasm_import_module = "host")]
+extern "C" {
+    fn curl(cmd: Vec<String>) -> MountedBinaryResult;
 }
